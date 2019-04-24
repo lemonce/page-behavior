c335d06895f5d5750a86c8f4f7d0e4a6c8df4a3e
DameNeko <phoensxar@gmail.com>
2019-04-25 07:51:13 +0800

    [FIXED] doubleClick

--- app/parser/python.js
+++ app/parser/python.js
@@ -3,14 +3,14 @@ const EOL = '\r\n';
 const bind = (identifier, value) => `${identifier} = ${value}`;
 const click = (identifier, value) => `${identifier}.click()`;
 const rightClick = (identifier, value) => `.context_click(${identifier}).perform()`;
-const doubleClick = (identifier, value) => `${identifier}.double_click()`;
+const doubleClick = (identifier, value) => `.double_click(${identifier}).perform()`;
 const input = (identifier, value) => `${identifier}.clear()` + EOL + `${identifier}.send_keys('${value}')`;
 const select = (identifier, value) => {};
 const check = (identifier, value) => `if ${identifier}.is_selected() == False: ${identifier}.send_keys(Keys.SPACE)`;
 const uncheck = (identifier, value) => `if ${identifier}.is_selected(): ${identifier}.send_keys(Keys.SPACE)`;
 
 const browser = { click, rightClick, doubleClick, input, select, check, uncheck };
-const actionChains = (driver, type) => type === 'rightClick' ? `ActionChains(${driver})` : '';
+const actionChains = (driver, type) => type.includes('Click') ? `ActionChains(${driver})` : '';
 const find = (driver, selector) => `${driver}.find_element_by_css_selector('${selector.trim()}')`;
 const switchTo = (driver, frame) => `${driver}.switch_to` + (frame === 'top' ? '.default_content()' : `.frame(${frame.trim()})`);
 
